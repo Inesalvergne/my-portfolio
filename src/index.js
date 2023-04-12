c425de9227f6dde3cd8f64cdc5109faaa512c00a
Ines Alvergne <i.alvergne@gmail.com>
2023-04-12 16:47:29 -0400

    mobile navbar logic

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
 import './style.css';
 console.log("hi from the console!");
 
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
 
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
+// Logic for mobile navbar
+const burger = document.querySelector('.navbar-burger');
+const menu = document.querySelector('.navbar-menu');
+const close = document.querySelector('.navbar-close')
 
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+burger.addEventListener('click', () => {
+  menu.classList.remove('hidden')
+})
 
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-});
+close.addEventListener('click', () => {
+  menu.classList.add('hidden')
+})
