11b944d78bf412307172dbd8493ea32bd6d9ff82
Ines Alvergne <i.alvergne@gmail.com>
2023-04-12 00:29:57 -0400

    home page is reponsive on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      'mobile': '320px',
+      // => @media (min-width: 480px) { ... }
+      'desktop': '1025px'
     },
     colors: {
       'dark-green': '#009486',
