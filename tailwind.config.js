6244cba6345e3548c920632520a7ef247fea4114
Ines Alvergne <i.alvergne@gmail.com>
2023-04-11 22:32:05 -0400

    add custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,32 @@ module.exports = {
   purge: ['./src/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      'dark-green': '#009486',
+      'light-green': '#D1E6E4',
+      'light-cream': '#FFFCF7',
+      'dark': '#161717'
+    },
+    fontFamily: {
+      mona: 'Mona Sans',
+      inter: 'Inter'
+    },
     extend: {},
   },
   variants: {
