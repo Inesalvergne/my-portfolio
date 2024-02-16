8832fe674d712827f06566d46560b7a952e479ca
Ines Alvergne <i.alvergne@gmail.com>
2024-02-16 20:53:07 +0100

    trying to troubleshoot

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
-import '../style.css';
 console.log("hi from index.js");
+import '../style.css';
 
 // Logic for mobile navbar
 const burger = document.querySelector('.navbar-burger');
 const menu = document.querySelector('.navbar-menu');
 const close = document.querySelector('.navbar-close')
+console.log(menu);
 
 burger.addEventListener('click', () => {
   menu.classList.remove('hidden')
